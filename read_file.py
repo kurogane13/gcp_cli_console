81e7107b46f98adfe9de53d95b8b8ead7f2bd36c
kurogane13 <akaisuzuki@gmail.com>
2023-02-17 14:15:01 -0300

    Removed __pychache__/ folder and files

--- read_file.py
+++
@@ -1,6 +0,0 @@
-def reading_from_file():
-  file_to_read=input('Provide file name to read: ')
-  openfile = open(file_to_read, 'r')
-  print(openfile.read())
-  openfile.close()
-reading_from_file()
